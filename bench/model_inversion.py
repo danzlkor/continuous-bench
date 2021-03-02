corrected the hessian calculation fixed a bug in test inversion

bench/model_inversion.py
@@ -13,7 +13,7 @@ import scipy.stats as st
 from typing import Union, Callable, List
 from progressbar import ProgressBar
 import argparse
-import numdifftools as nd
+#import numdifftools as nd
 
 
 def log_prior(params, priors):
@@ -35,34 +35,29 @@ def log_posterior_sig(params, priors, model, y, noise_level):
     else:
         ll = log_likelihood_sig(param_dict, model, y, noise_level)
 
-    return prior * 0 + ll
+    return prior + ll
 
 
 def map_fit_sig(model: Callable, priors: dict, y: np.ndarray, noise_level):
     x0 = np.array([v.mean() for v in priors.values()])
-    # bounds = [v.interval(1 - 1e-2) for v in priors.values()]
+    bounds = [v.support() for v in priors.values()]
 
     f = lambda x: -log_posterior_sig(x, priors, model, y, noise_level)
-    p = optimize.minimize(f, x0=x0, options={'disp': False})
-    h = hessian(f, p.x)
-    h = nd.Hessian(f)(p.x)
-    std = 1/np.sqrt(abs(np.diag(h)))
+    p = optimize.minimize(f, x0=x0, bounds=bounds, options={'disp': False})
+    h = hessian(f, p.x, dp=1e-2)
+    # h = nd.Hessian(f)(p.x)
+    std = np.sqrt(np.diag(abs(np.linalg.inv(h))))
     return p.x, std
 
 
 def grad(f, p, dp=1e-6):
-    dp *= np.abs(p[np.nonzero(p)]).min()
-    fp = f(p)
-    if np.isscalar(fp):
-        l = 1
-    else:
-        l = len(fp)
-    g = np.zeros((len(p), l))
+    dp = np.maximum(1e-20, abs(p * dp))
+    g = []
     for i in range(len(p)):
         pi = np.zeros(len(p))
-        pi[i] = dp
-        g[i] = np.squeeze((f(p + pi) - fp)/dp)
-    return g
+        pi[i] = dp[i]
+        g.append(np.squeeze((f(p + pi) - f(p-pi))/(2 * dp[i])))
+    return np.stack(g, axis=0)
 
 
 def hessian(f, p, dp=1e-6):

bench/tests/test_model_inversion.py
@@ -18,14 +18,14 @@ def test_parameter_estimation():
     actual = np.array([1, 2, 2])
     noise_level = 1e-3
     data = toy_model(x, *actual)
-    noisy_data = data + noise_level * np.rand
+    noisy_data = data + noise_level * np.random.randn(*data.shape)
 
     fits, _ = mi.map_fit_sig(func, param_priors, noisy_data, noise_level)
     np.testing.assert_allclose(actual, fits, rtol=1e-2)
 
 
 def test_std_estimation():
-    x = np.array([1, -3, 4])[:, np.newaxis]
+    x = np.array([1, 2, 3])[:, np.newaxis]
     func = lambda params: toy_model(x, **params)
     actual_params = np.array([1, 2, 2])
     noise_level = 1e-3
@@ -37,13 +37,13 @@ def test_std_estimation():
 
     a = np.array([[p ** 2, p, 1] for p in x[:, 0]])
     a_inv = np.linalg.inv(a)
-    pe_a = np.squeeze(a_inv @ noisy_data)
+    pe_a = np.squeeze(a_inv @ data)
 
     cov = a_inv.dot(a_inv.T) * (noise_level ** 2)
-    stda = np.diagonal(cov)
+    stda = np.sqrt(np.diagonal(cov))
 
-    np.testing.assert_allclose(pe_a, actual_params, rtol=1e-2)
-    np.testing.assert_allclose(stda, stde, rtol=1e-2)
+    np.testing.assert_allclose(pe_a, actual_params, rtol=1e-3)
+    np.testing.assert_allclose(stda, stde, rtol=1e-7)
 
 
 def test_confmats():