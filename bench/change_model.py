fixed a bug with odi (negativr sign required for k1, k2 in bingham zeppelin) removed check for degenerate regressed covariances. in normalized summary dy and sigma_n are optional

bench/change_model.py
@@ -136,7 +136,7 @@ class MLChangeVector:
             tril_idx = np.tril_indices(self.mean_y.shape[-1])
             self.diag_idx = np.argwhere(tril_idx[0] == tril_idx[1])
 
-    def distribution(self, y, lam=1e-6):
+    def distribution(self, y, lam=1e-12):
         """
         estimate mu and sigma from y using the trained regression models.
         :param y: normalized baseline measurements
@@ -147,12 +147,12 @@ class MLChangeVector:
         yf_mu = self.mu_feature_extractor.fit_transform(y - self.mean_y)
         yf_sigma = self.sigma_feature_extractor.fit_transform(y - self.mean_y)
         mu, sigma_inv, _ = regression_model(yf_mu, yf_sigma, self.mu_weight, self.sig_weight, self.diag_idx)
-
-        if np.linalg.cond(sigma_inv) < 1 / np.sys.float_info.epsilon:
-            sigma = np.linalg.inv(sigma_inv)
-        else:
-            sigma = np.linalg.inv(sigma_inv + lam * np.eye(sigma_inv.shape[-1]))
-            warnings.warn('estimated inverse matrix is singular.')
+        sigma = np.linalg.inv(sigma_inv)
+        # if np.linalg.cond(sigma_inv) < 1 / np.sys.float_info.epsilon:
+        #     sigma = np.linalg.inv(sigma_inv)
+        # else:
+        #     sigma = np.linalg.inv(sigma_inv + lam * np.eye(sigma_inv.shape[-1]))
+        #     warnings.warn('estimated inverse matrix is singular.')
         return mu, sigma
 
     def log_lh(self, dv, y, dy, sigma_n):
@@ -232,7 +232,7 @@ class ChangeModel:
         """
 
         print(f'running inference for {data.shape[0]} samples ...')
-        y, dy, sn = summary_measures.normalize_summaries(data, delta_data, sigma_n, self.summary_names)
+        y, dy, sn = summary_measures.normalize_summaries(data, self.summary_names, delta_data, sigma_n)
         lls, peaks = self.compute_log_likelihood(y, dy, sn, parallel=parallel, integral_bound=10)
         priors = np.array([m.prior for m in self.models])  # the 1 is for empty set
         priors = priors / priors.sum()
@@ -346,7 +346,7 @@ class ChangeModel:
 
     def estimate_quality_of_fit(self, y1, dy, sigma_n, predictions, peaks):
         dv = np.array([p[i] for i, p in zip(predictions, peaks)])
-        y1, dy, sigma_n = summary_measures.normalize_summaries(y1, dy, sigma_n, self.summary_names)
+        y1, dy, sigma_n = summary_measures.normalize_summaries(y1, self.summary_names, dy, sigma_n)
         dists = [self.models[p].distribution(y) for p, y in zip(predictions, y1)]
         mu = np.stack([np.squeeze(d[0]) for d in dists], axis=0)
         sigma = np.stack([np.squeeze(d[1]) for d in dists], axis=0)
@@ -1008,7 +1008,7 @@ def plot_conf_mat(conf_mat, param_names, f_name=None, title=None):
     ax.set_xticklabels(labels=param_names, rotation=45, fontdict={'size': 12})
     ax.set_yticklabels(labels=param_names, rotation=45, fontdict={'size': 12})
     ax.set_xlabel('Actual change', fontdict={'size': 16})
-    ax.set_ylabel('Predicted Change', fontdict={'size': 16})
+    ax.set_ylabel('Inferred Change', fontdict={'size': 16})
     if title is not None:
         plt.title(title)
     if f_name is not None:

bench/diffusion_models.py
@@ -47,7 +47,7 @@ prior_distributions = dict(
                   'd_a_in': stats.truncnorm(loc=dif_coeff, scale=.3, a=-dif_coeff / 0.3, b=np.Inf),
                   'd_a_ex': stats.truncnorm(loc=dif_coeff, scale=.3, a=-dif_coeff / 0.3, b=np.Inf),
                   'tortuosity': stats.uniform(loc=0.01, scale=.98),
-                  'odi': stats.uniform(loc=.01, scale=.98),
+                  'odi': stats.uniform(loc=0, scale=1),
                   },
 
     bingham_noddi={'s_iso': stats.truncnorm(loc=.5, scale=.2, a=-.5 / .2, b=np.Inf),
@@ -205,20 +205,20 @@ def bingham_zeppelin(bval=0., bvec=np.array([[0, 0, 1]]), d_a=1., d_r=0.,
     k1 = 1 / np.tan(odi * np.pi / 2)
     k2 = 1 / np.tan(odi2 * np.pi / 2)
     b_diag = np.zeros(k1.shape + (3, 3))
-    b_diag[..., 0, 0] = k1
-    b_diag[..., 1, 1] = k2
+    b_diag[..., 0, 0] = -k1
+    b_diag[..., 1, 1] = -k2
 
     if r.shape[0] == 1:
         bing_mat = np.array([r[0].T @ b_diag[i] @ r[0] for i in range(b_diag.shape[0])])
     elif r.shape[0] == n_samples:
         bing_mat = np.array([r[i].T @ b_diag[i] @ r[i] for i in range(b_diag.shape[0])])
 
-    denom = hyp_sapprox(np.linalg.eigvalsh(bing_mat)[..., ::-1])
+    denom = hyp_sapprox(np.stack([np.zeros_like(k1), -k2, -k1], -1))  # np.linalg.eigvalsh(bing_mat)[..., ::-1])
     q = bing_mat[:, np.newaxis, :, :] - (bval * (d_a - d_r)[..., np.newaxis])[..., np.newaxis, np.newaxis] * \
         ((bvec[:, np.newaxis, :] * bvec[:, :, np.newaxis])[np.newaxis, ...])
     num = hyp_sapprox(np.linalg.eigvalsh(q)[..., ::-1]) * np.exp(-d_r[..., np.newaxis] * bval)
 
-    #for bval_, g in zip(bval, bvec):
+    # for bval_, g in zip(bval, bvec):
     #    q = bing_mat - bval_ * (d_a - d_r) * g[:, np.newaxis].dot(g[np.newaxis, :])
     #    num = hyp_sapprox(np.linalg.eigvalsh(q)[::-1]) * np.exp(-d_r * bval_)
     #    s.append(num)
@@ -243,11 +243,11 @@ def watson_zeppelin_numerical(bval=0, bvec=np.array([[0, 0, 1]]), d_a=1., d_r=0.
     """
     assert odi > 0, 'odis must be positive'
 
-    if np.isscalar(bval):
-        bval = bval * np.ones(bvec.shape[0])
-
     if bvec.ndim == 1:
         bvec = bvec[np.newaxis, :]
+
+    if np.isscalar(bval):
+        bval = bval * np.ones(bvec.shape[0])
 
     k = 1 / np.tan(odi * np.pi / 2)
     mu = np.array(spherical2cart(theta, phi))
@@ -632,7 +632,7 @@ def simulate_signal(model, acq, model_params):
 
     :param model: function object from diffusion models
     :param acq: Acquisition object containing acquisition parameters
-    :param params: dictionary of model parameter with shape S
+    :param model_params: dictionary of model parameter with shape S
     :return: diffusion signal (S..., ndirs)
     """
     bvals = np.array([s.bval for s in acq.shells])

bench/summary_measures.py
@@ -151,7 +151,7 @@ def fit_summary_single_subject(subj_idx: str, diff_add: str, xfm_add: str, bvec_
     if os.path.exists(fname):
         print(f'Summary measurements for subject {subj_idx} alredy exists.')
         return 2
-    
+
     print('diffusion data address:' + diff_add)
     print('xfm address:' + xfm_add)
     print('bvec address: ' + bvec_add)
@@ -174,12 +174,12 @@ def fit_summary_single_subject(subj_idx: str, diff_add: str, xfm_add: str, bvec_
             for t in names:
                 f.write("%s\n" % t)
             f.close()
-           
+
     print(f'Summary measurements for subject {subj_idx} computed')
     return 1
 
 
-def normalize_summaries(y1, dy, sigma_n, names):
+def normalize_summaries(y1: np.ndarray, names, dy=None, sigma_n=None):
     """
     Normalises summary measures for all subjects. (divide by average attenuation)
     :param names: name of summaries, is required for knowing how to normalize
@@ -188,29 +188,44 @@ def normalize_summaries(y1, dy, sigma_n, names):
     :param sigma_n: array or list of covariance matrices
     :return: normalised summaries
     """
-    if not len(names) == y1.shape[-1]:
-        raise ValueError(f'Number of summary measurements doesnt match.'
-                         f'\n Expected {len(names)} measures but got {y1.shape[2]}.')
-
-    y1, dy, sigma_n = [np.array(v) for v in (y1, dy, sigma_n)]
+    assert len(names) == y1.shape[-1], f'Number of summary measurements doesnt match. ' \
+                                       f'Expected {len(names)} measures but got {y1.shape[-1]}.'
+    y1 = np.array(y1)
     b0_idx = names.index('b0_mean')
     summary_type = [l.split('_')[1] for l in names]
     mean_b0 = np.atleast_1d(y1[..., b0_idx])
     y1_norm = np.zeros_like(y1)
-    dy_norm = np.zeros_like(dy)
-    sigma_n_norm = sigma_n.copy()
 
     for smm_idx, l in enumerate(summary_type):
         if l == 'mean':
             y1_norm[..., smm_idx] = y1[..., smm_idx] / mean_b0
-            dy_norm[..., smm_idx] = dy[..., smm_idx] / mean_b0
-            sigma_n_norm[..., smm_idx, :] = sigma_n_norm[..., smm_idx, :] / mean_b0[:, np.newaxis]
-            sigma_n_norm[..., :, smm_idx] = sigma_n_norm[..., :, smm_idx] / mean_b0[:, np.newaxis]
         else:
             y1_norm[..., smm_idx] = y1[..., smm_idx] / (mean_b0 ** 2)
-            dy_norm[..., smm_idx] = dy[..., smm_idx] / (mean_b0 ** 2)
-            sigma_n_norm[..., smm_idx, :] = sigma_n_norm[..., smm_idx, :] / (mean_b0[:, np.newaxis] ** 2)
-            sigma_n_norm[..., :, smm_idx] = sigma_n_norm[..., :, smm_idx] / (mean_b0[:, np.newaxis] ** 2)
-
     y1_norm = np.delete(y1_norm, b0_idx, axis=-1)
-    return y1_norm, dy_norm, sigma_n_norm
+    res = [y1_norm]
+    if dy is not None:
+        dy=np.array(dy)
+        dy_norm = np.zeros_like(dy)
+        for smm_idx, l in enumerate(summary_type):
+            if l == 'mean':
+                dy_norm[..., smm_idx] = dy[..., smm_idx] / mean_b0
+            else:
+                dy_norm[..., smm_idx] = dy[..., smm_idx] / (mean_b0 ** 2)
+        res.append(dy_norm)
+
+    if sigma_n is not None:
+        sigma_n = np.array(sigma_n)
+        sigma_n_norm = sigma_n.copy()
+        for smm_idx, l in enumerate(summary_type):
+            if l == 'mean':
+                sigma_n_norm[..., smm_idx, :] = sigma_n_norm[..., smm_idx, :] / mean_b0[:, np.newaxis]
+                sigma_n_norm[..., :, smm_idx] = sigma_n_norm[..., :, smm_idx] / mean_b0[:, np.newaxis]
+            else:
+                sigma_n_norm[..., smm_idx, :] = sigma_n_norm[..., smm_idx, :] / (mean_b0[:, np.newaxis] ** 2)
+                sigma_n_norm[..., :, smm_idx] = sigma_n_norm[..., :, smm_idx] / (mean_b0[:, np.newaxis] ** 2)
+        res.append(sigma_n_norm)
+
+    if dy is None and sigma_n is None:
+        return y1_norm
+    else:
+        return tuple(res)