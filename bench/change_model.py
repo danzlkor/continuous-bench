e2c41dcf57d89c3928210568aa092b27356f3743
Hossein <hossein.rafipoor@ndcn.ox.ac.uk>
2021-06-14 16:46:43 +0200

    fixed a bug

--- bench/change_model.py
+++ bench/change_model.py
@@ -1004,7 +1004,7 @@ def estimate_median(pdf: Callable, bounds, n_samples=int(1e3)):
     :return:
     """
     x = np.linspace(bounds[0], bounds[1], n_samples)
-    lh = np.array([0.] + [np.exp(pdf(x_)) for x_ in x])
+    lh = np.array([0.] + [pdf(x_) for x_ in x])
     p_idx = np.argwhere(np.cumsum(lh) / lh.sum() < 0.5)[-1]
     return x[p_idx]
 
