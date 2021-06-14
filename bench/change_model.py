5a8f7133f89636430983574d3dc20cb51083008e
Hossein <hossein.rafipoor@ndcn.ox.ac.uk>
2021-06-14 17:21:20 +0200

    refactor

--- bench/change_model.py
+++ bench/change_model.py
@@ -1004,7 +1004,7 @@ def estimate_median(pdf: Callable, bounds, n_samples=int(1e3)):
     :return:
     """
     x = np.linspace(bounds[0], bounds[1], n_samples)
-    p = np.array([pdf(x_) for x_ in x])
+    p = np.array([np.squeeze(pdf(x_)) for x_ in x])
     p_idx = np.argwhere(np.cumsum(p) > 0.5 * p.sum())[0]
     return x[p_idx]
 
