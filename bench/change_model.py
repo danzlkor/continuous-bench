140a2841ebd9bf5bf8826a5f723f54f6e82c5506
Hossein <hossein.rafipoor@ndcn.ox.ac.uk>
2021-07-14 12:33:04 +0100

    fixed a bug for shape

--- bench/change_model.py
+++ bench/change_model.py
@@ -56,9 +56,9 @@ class MLChangeVector:
         self.mu_feature_extractor = PolynomialFeatures(degree=self.mu_poly_degree)
         self.sigma_feature_extractor = PolynomialFeatures(degree=self.sigma_poly_degree)
 
-        if self.mean_y is not None:
-            tril_idx = np.tril_indices(self.mu_weight.shape[-1])
-            self.diag_idx = np.argwhere(tril_idx[0] == tril_idx[1])
+
+        tril_idx = np.tril_indices(self.mu_weight.shape[-1])
+        self.diag_idx = np.argwhere(tril_idx[0] == tril_idx[1])
 
     def distribution(self, y, lam=1e-12):
         """
